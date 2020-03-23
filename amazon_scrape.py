b59d820c54a2a63f8ce155a089e8f6a5e2e86456
RamVignesh B <ramvigneshgreat@pec.edu>
2020-03-23 19:50:18 +0530

    Added Comments

--- amazon_scrape.py
+++ amazon_scrape.py
@@ -5,34 +5,33 @@ import smtplib
 import time
 from datetime import datetime
 
-URL= 'https://www.amazon.in/Lenovo-Legion-Graphics-Windows-81SY00CKIN/dp/B07W6H9YM9/ref=sr_1_3?keywords=y540&qid=1578141911&sr=8-3'
+URL= 'https://www.amazon.in/Lenovo-Legion-Graphics-Windows-81SY00CKIN/dp/B07W6H9YM9/ref=sr_1_3?keywords=y540&qid=1578141911&sr=8-3' #product URL
 
 headers = {"User-Agent":'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'}
 
-
+mail_id = 'ramvignesh.codes@gmail.com' #The mail ID from where the mail will be sent
+app_pass = 'whvpktonrsznfnxz' #Generated "app-specific password" from Gmail Account Settings 
+recipient = 'ramvigneshgreat@gmail.com' #where the email will be sent to
 
 def check_price():
-    target_price = 63000
+    target_price = 63000 #Email will be sent once the products price reach below this value
     page = requests.get(URL, headers=headers)
     soup = bs(page.content, 'html.parser')
     title = soup.find(id="productTitle").get_text()
     price = soup.find(id="priceblock_ourprice").get_text()
-    ref_price = Decimal(price[2:].replace(",",""))
+    ref_price = Decimal(price[2:].replace(",","")) #the actual price of the product
     print(datetime.now().strftime("%H:%M:%S"),'Checking price.....')
     print(price)
     if(ref_price <= target_price):
         send_mail(title,ref_price)
 
-def send_mail(title1,ref_price1):
+def send_mail(title1,ref_price1): #function to send the mail
     title = title1
     ref_price = ref_price1
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
-    mail_id = 'ramvignesh.codes@gmail.com'
-    app_pass = 'whvpktonrsznfnxz'
-    recipient = 'ramvigneshgreat@gmail.com'
     server.login(mail_id, app_pass)
 
     subject = 'Price is slashed ^_^'
